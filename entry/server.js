c7c304aeb1446589ed534683642008c09d2c3937
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-24 09:17:28 -0500

    project database util

--- entry/server.js
+++ entry/server.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const vntUtil = require("../util/vntUtil");
+const projectDBUtil = require("../util/projectDBUtil");
 
 const profileRoutes = require("../profile/routes");
 const loginRoutes = require("../login/routes");
@@ -15,17 +15,17 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 
-mongoose.connect(vntUtil.MONGODB_CONN_STRING, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(projectDBUtil.MONGODB_CONN_STRING, {useUnifiedTopology: true, useNewUrlParser: true});
 const connection = mongoose.connection;
 
 connection.once('open', function () {
   console.log(`---- MongoDB database connection established successfully`);
 })
 
-app.use(vntUtil.PROFILE_API_PREFIX, profileRoutes);
-app.use(vntUtil.USER_API_PREFIX, loginRoutes);
-app.use(vntUtil.POST_API_PREFIX, postRoutes);
+app.use(projectDBUtil.PROFILE_API_PREFIX, profileRoutes);
+app.use(projectDBUtil.USER_API_PREFIX, loginRoutes);
+app.use(projectDBUtil.POST_API_PREFIX, postRoutes);
 
-app.listen(process.env.PORT || vntUtil.PORT, function () {
-  console.log("---- Server is running on Port: " + vntUtil.PORT);
+app.listen(process.env.PORT || projectDBUtil.PORT, function () {
+  console.log("---- Server is running on Port: " + projectDBUtil.PORT);
 });
